2db8e0ad45b895a6838107270d5262b3ba29f264
dh4rry <harald.gutbrod+github@gmail.com>
2020-04-13 16:24:15 +0200

    fix open_sap

--- plugin.video.open_sap/default.py
+++ plugin.video.open_sap/default.py
@@ -16,8 +16,6 @@ def login():
     addon = xbmcaddon.Addon('plugin.video.open_sap')
     user = addon.getSetting('Username')
     pw = addon.getSetting('Password')
-    if user == "":
-        addon.openSettings()
     r = s.get('https://open.sap.com/sessions/new')
     soup = BeautifulSoup(r.content, 'html.parser')
     auth_token = soup.find("input", { "name": "authenticity_token"} )['value']
@@ -37,6 +35,7 @@ def get_enrollments():
             found = True
             addDirectoryItem(plugin.handle, plugin.url_for(show_course, data['id']), ListItem(data['attributes']['title']), True)
             print(data['id'])
+
     if found == False:
         addDirectoryItem(plugin.handle, 'plugin://plugin.video.open_sap/', ListItem('No enrollment or wrong login data'), True)
     
@@ -48,24 +47,17 @@ def show_course(course_id):
     j = json.loads(r.content)
     for data in j['data']:
         if data['attributes']['content_type'] == 'video':
-            show_stream(data['relationships']['content']['data']['id'])
+            show_stream(data['relationships']['content']['data']['id'], data['attributes']['title'])
     endOfDirectory(plugin.handle)
 
-def show_stream(video_id):
-    try:
-        r = s.get('https://open.sap.com/api/v2/videos/' + video_id)
-        j = json.loads(r.content)
-        liz = xbmcgui.ListItem( j['data']['attributes']['title'], iconImage='', thumbnailImage=j['data']['attributes']['single_stream']['thumbnail_url'])
-        infoLabels = {"Title": j['data']['attributes']['title']}
-        liz.setInfo(type="Video", infoLabels=infoLabels)
-        liz.setProperty('IsPlayable', 'true')
-        addDirectoryItem(handle=plugin.handle, url=j['data']['attributes']['single_stream']['hd_url'], listitem=liz)
-        #print(j['data']['attributes']['title'])
-        #print('Dauer:' + str(j['data']['attributes']['duration']))
-        #print('Stream:' +  j['data']['attributes']['single_stream']['hd_url'])
-        #print('Thumbnail' + j['data']['attributes']['single_stream']['thumbnail_url'])
-    except:
-        print('Fehler bei video' + video_id)
+def show_stream(video_id, title):
+    r = s.get('https://open.sap.com/api/v2/videos/' + video_id)
+    j = json.loads(r.content)
+    liz = xbmcgui.ListItem( title, iconImage='', thumbnailImage=j['data']['attributes']['single_stream']['thumbnail_url'])
+    infoLabels = {"Title": 	title}
+    liz.setInfo(type="Video", infoLabels=infoLabels)
+    liz.setProperty('IsPlayable', 'true')
+    addDirectoryItem(handle=plugin.handle, url=j['data']['attributes']['single_stream']['hd_url'], listitem=liz)
 
 
 #print('enrollments:')
--- publish_addons.sh
+++ publish_addons.sh
@@ -1,4 +1,4 @@
 python addons_xml_generator.py
 git add *
-git commit -m $1
+git commit -a $1
 git push
